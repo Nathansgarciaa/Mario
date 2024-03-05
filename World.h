1dd8a2afa9d3c22c5bfaf7e8014d1a1cbc5ebe73
Nathan Garcia <125214535+Nathansgarciaa@users.noreply.github.com>
2024-03-05 13:09:59 -0800

    Add files via upload

---
+++ World.h
@@ -0,0 +1,18 @@
+#ifndef WORLD_H
+#define WORLD_H
+class World{
+    public:
+        World();
+        ~World();
+        World(int levels);
+        int levelAmount;
+        int levelCount;
+        Levels
+        int getLevel();
+        void nextLevel();
+        void endGame();
+        
+    
+        
+};
+#endif
