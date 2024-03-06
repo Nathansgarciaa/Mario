455b1ee7e33c8c96a4ed4f47ae231d4c70df2078
Nathansgarciaa <ngarcia1@chapmanedu>
2024-03-05 17:44:08 -0800

    update

--- Mario.h
+++ Mario.h
@@ -1,6 +1,20 @@
 #ifndef MARIO_H
 #define MARIO_H
 class Mario{
+    public:
+        unsigned int v;//lives
+        unsigned int coinCount;
+        unsigned int pl;
+        
+        void moveLeft();
+        void moveRight();
+        void moveUp();
+        void moveDown();
+        
+        void interaction(char d);//char d is the represenation of the other objects in the grid
+        
+
+
 
 }
 ;
