e14f69903a5aa3c42175d43e1cce24bc5312a7ff
daniielahn <91645852+daniielahn@users.noreply.github.com>
2024-03-06 17:11:15 -0800

    Add files via upload

--- Level.h
+++ Level.h
@@ -1,17 +1,38 @@
+//create grid, fill grid with items, enemies, 
+
+
+//funcs
+//getValue of position
+//setValue of position
+
 #ifndef LEVEL_H
-#define LEVEL_H
+#include <fstream>
+#include <iostream>
+using namespace std;
+
 class Level{
-    public:
+    public: 
         Level();
+        Level(int sizeOfGrid);
         ~Level();
-        
+        void printGrid();
+        void fillGrid();
+        void placeBoss();
+        void placeWarp();
+        void placeMario();
+        void getCoord(char value, int& row, int &col);
+        char getPos(int i, int j);
+        void setPos(int i, int j, char value);
 
 
-    private:
-        int n;
+    private: 
+        int N; //size of grid
+        int M; // size of grid
+        int row;
+        int col;
+        char *grid; //2D grid
 
-}
-;
 
+};
 
 #endif
